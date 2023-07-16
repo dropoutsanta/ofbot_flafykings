0a42563e963e471cd5479c19f4adbe5975a973de
thierry99@videotron.ca <thierry@thierrys-air.lan>
2023-07-16 13:40:21 -0400

    commit

--- api.py
+++ api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from telegram import Bot, Update
+from app2 import setup_bot
 
 
 
@@ -10,7 +11,8 @@ bots = {}
 @app.route('/add_bot', methods=['POST'])
 def add_bot():
     print("RUNNNNNNNNING")
-    return 200
+
+    return 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
