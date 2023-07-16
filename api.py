bb0914ed2b413fc0b5758ccd7d9f58d84111dd1e
thierry99@videotron.ca <thierry@thierrys-air.lan>
2023-07-16 13:55:35 -0400

    commit

--- api.py
+++ api.py
@@ -3,14 +3,21 @@ from telegram import Bot, Update
 from app2 import setup_bot
 import app2
 
-
 app = Flask(__name__)
 bots = {}
 
 @app.route('/add_bot', methods=['POST'])
 def add_bot():
     print("RUNNNNNNNNING")
-    return jsonify({"message": "Bot added successfully"}), 200
+    bot_data = request.json
+    
+    if bot_data:
+        updater = setup_bot(bot_data)
+        app2.updaters.append(updater)
+        return jsonify({"message": "Bot added successfully"}), 200
+    else:
+        return jsonify({"message": "Bot not added successfully"}), 200
+   
 
         # Here you could add the logic to restart or update your service as necessary
 
